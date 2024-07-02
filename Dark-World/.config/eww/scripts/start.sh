eaec55a1675e23c2c30a4c9779fa6a69c0fda836
MrVivekRajan <rajanvivek9999@gmail.com>
2024-07-02 19:28:51 +0000

    New Theme

---
+++ Dark-World/.config/eww/scripts/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pkill eww
+eww daemon
+eww open bg_widgets
+eww open apps
---
+++ Dark-World/.config/hypr/scripts/songdetail.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+song_info=$(playerctl metadata --format '{{title}}  {{artist}}')
+
+echo "$song_info" 
